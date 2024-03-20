77457b6a590423d8c92dac5603a68d799d2f4a9b
titoironman <titoironman@msn.com>
2024-03-21 00:27:24 +0300

    Task 5 solution added

--- 5.js
+++ 5.js
@@ -5,7 +5,8 @@
 так, чтобы в объекте ключами выступали значения из массива en, а значениями 
 объекта являлись значения из массива ru.
 */
-
+console.log("");
+console.log("Задание 5:");
 const en = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
 const ru = [
   "понедельник",
@@ -17,4 +18,14 @@ const ru = [
   "воскресенье",
 ];
 
-// Здесь пишем решение, данный комментарий необходимо стереть.
+function convertToObj(en, ru) {
+  if (en.length != ru.length ||
+    en.length == 0 ||
+    en.length == 0) {
+    return null;
+  }
+  let obj = {};
+  en.forEach((k, i) => { obj[k] = ru[i] })
+  return obj;
+}
+console.log(convertToObj(en, ru))
